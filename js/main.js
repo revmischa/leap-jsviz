fc1b2c05b3f2071f079e25c4fa2c59c6623f5dd1
Mischa S <revmischa@cpan.org>
2013-05-05 15:10:51 -0700

    very very simple fingertip visualizer with Raphael.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
-var cb = {};
+var cb = {
 
-cb.gotFrame = function(frame) {
-	console.log(frame);
-	cb.controller.removeListener('animationFrame', cb.gotFrame);
-};
+	initCanvas: function() {
+		cb.paper = Raphael("viz", 640, 480);
+	},
 
-cb.connected = function(frame) {
-	console.log(frame);
+	initLeap: function() {
+		// Leap controller initialization options
+		var controllerOptions = { enableGestures: true };
+		var controller = new Leap.Controller(controllerOptions);
+		cb.controller = controller;
+
+		cb.frameHandlerRef = controller.on('animationFrame', cb.gotFrame);
+		controller.connect();
+	},
+
+	gotFrame: function(frame) {
+		if (! frame.hands.length) return;
+
+		var paper = cb.paper;
+		var paperWidth = $("#viz").width();
+		var paperHeight = $("#viz").height();
+		paper.clear();
+
+		// draw fingers
+		for (var i = 0; i < frame.hands.length; i++) {
+			var hand = frame.hands[i];
+			for (var i = 0; i < hand.pointables.length; i++) {
+				var pointable = hand.pointables[i];
+
+				var pos = pointable.tipPosition;
+				var pointX = pos.x + 200;
+				var pointY = 400 - pos.y;
+				var pointRadius = (pos.z + 200) / 10;
+
+				var circle = paper.circle(pointX, pointY, pointRadius);
+				circle.attr("stroke", "#00c");
+			}
+		}
+	}
 };
 
 $(function() {
-	// Leap controller initialization options
-	var controllerOptions = { enableGestures: true };
-	var controller = new Leap.Controller(controllerOptions);
-	cb.controller = controller;
-
-	cb.frameHandlerRef = controller.on('connectionFrame', cb.connected);
-	cb.frameHandlerRef = controller.on('animationFrame', cb.gotFrame);
-	controller.connect();
+	cb.initCanvas();
+	cb.initLeap();
 });
 
