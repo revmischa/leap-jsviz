54b7ef86c178357900069e9f3fa828d3286b8ef2
Mischa S <revmischa@cpan.org>
2013-05-07 20:55:51 -0700

    three.js skeleton

--- js/main.js
+++ js/main.js
@@ -1,42 +1,65 @@
 var cb = {
+	scene: new THREE.Scene(),
+	camera: new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+	renderer: new THREE.WebGLRenderer(),
 
 	initCanvas: function() {
-		cb.paper = Raphael("viz", 640, 480);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(this.renderer.domElement);
+
+		var geometry = new THREE.PlaneGeometry(5, 5);
+		var camTexture = new THREE.Texture();
+		var camLoader = new THREE.ImageLoader();
+		camLoader.addEventListener('load', function (e) {
+			camTexture.image = e.content;
+			camTexture.needsUpdate = true;
+		});
+		camLoader.crossOrigin = true;
+		camLoader.load('img/test.jpeg');
+		var material = new THREE.MeshBasicMaterial({ 'map': camTexture });
+		this.cube = new THREE.Mesh(geometry, material);
+		this.scene.add(this.cube);
+
+		this.camera.position.z = 5;
+
+		this.renderCanvas();
+		$(window).on('resize', $.proxy(cb.onWindowResize, this));
+	},
+
+	onWindowResize: function(e) {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	},
+
+	renderCanvas: function() {
+		var self = this;
+		requestAnimationFrame(function () { self.renderCanvas() });
+
+		var translationDelta = this.translationDelta;
+		console.log(translationDelta);
+
+		this.renderer.render(this.scene, this.camera);
 	},
 
 	initLeap: function() {
 		// Leap controller initialization options
 		var controllerOptions = { enableGestures: true };
 		var controller = new Leap.Controller(controllerOptions);
-		cb.controller = controller;
+		this.controller = controller;
 
-		cb.frameHandlerRef = controller.on('animationFrame', cb.gotFrame);
+		this.frameHandlerRef = controller.on('animationFrame', $.proxy(cb.gotFrame, this));
 		controller.connect();
 	},
 
 	gotFrame: function(frame) {
-		if (! frame.hands.length) return;
+		// get translation
+		var lastFrame = this.controller.frame(1);
+		if (! lastFrame) return;
 
-		var paper = cb.paper;
-		var paperWidth = $("#viz").width();
-		var paperHeight = $("#viz").height();
-		paper.clear();
-
-		// draw fingers
-		for (var i = 0; i < frame.hands.length; i++) {
-			var hand = frame.hands[i];
-			for (var i = 0; i < hand.pointables.length; i++) {
-				var pointable = hand.pointables[i];
-
-				var pos = pointable.tipPosition;
-				var pointX = pos.x + 200;
-				var pointY = 400 - pos.y;
-				var pointRadius = (pos.z + 200) / 10;
-
-				var circle = paper.circle(pointX, pointY, pointRadius);
-				circle.attr("stroke", "#00c");
-			}
-		}
+		var translation = frame.translation(lastFrame);
+		this.translationDelta = translation;
 	}
 };
 
